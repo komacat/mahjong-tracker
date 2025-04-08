cbbe8dbb232ea84691c0046bad123cd3feec9d9b
komacat <yeahdsj@gmail.com>
2025-04-08 00:27:17 +0000

    wip

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
     import type { EventAttendee, User } from '@prisma/client'
-    import UserAvatar from './UserAvatar.svelte';
+    import UserAvatar from './UserAvatar.svelte'
     import type { PageData } from './$types'
-    
-    export let data: PageData;
-    export let join: (user: string) => Promise<void>;
+
+    export let data: PageData
+    export let join: (user: string) => Promise<void>
 
     let usersList: User[] = data.users
 
-    let query: string = ""
+    let query: string = ''
 
     function search(query: string): User[] {
-        return usersList.filter(user => {
-            const attendee = data.attendee.find((attendee: EventAttendee) => attendee.userId === user.id)
+        return usersList.filter((user) => {
+            const attendee = data.attendee.find(
+                (attendee: EventAttendee) => attendee.userId === user.id
+            )
             return (
                 user.username.toLowerCase().includes(query.toLowerCase()) &&
                 (!attendee || attendee.status === 'REJECTED')
@@ -28,14 +30,20 @@
         <input
             type="text"
             bind:value={query}
-            placeholder="Search by Discord Username"
+            placeholder="Search or add by Discord username"
             class="peer w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
             on:focus={() => {
                 searchResult = search(query)
             }}
         />
+        <button
+            type="button"
+            class="material-symbols-rounded ml-auto flex flex-row items-center rounded-lg bg-blue-500 p-2 text-white"
+        >
+            add</button
+        >
         <div
-            class="absolute -left-1 top-10 hidden w-[calc(100%-0.5rem)] flex-col divide-y rounded-lg border border-gray-300 bg-gray-50 p-2 shadow-lg peer-focus:flex"
+            class="absolute -left-1 top-10 hidden w-[calc(100%-4rem)] flex-col divide-y rounded-lg border border-gray-300 bg-gray-50 p-2 shadow-lg peer-focus:flex"
         >
             {#if searchResult.length > 0}
                 {#each searchResult as user}
