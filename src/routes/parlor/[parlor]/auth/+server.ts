d5e154e0f833d22d5e61efb2a93b515a80fe1896
komacat <yeahdsj@gmail.com>
2025-04-15 21:30:55 +0000

    format

--- src/routes/parlor/[parlor]/auth/+server.ts
+++ src/routes/parlor/[parlor]/auth/+server.ts
@@ -1,7 +1,7 @@
-import { isParlorAdmin } from "$lib/server/auth";
-import { getSessionId } from "$lib/server/session";
-import { getUser } from "$lib/server/user";
-import { error } from "@sveltejs/kit";
+import { isParlorAdmin } from '$lib/server/auth'
+import { getSessionId } from '$lib/server/session'
+import { getUser } from '$lib/server/user'
+import { error } from '@sveltejs/kit'
 
 export const POST = async ({ cookies, params }) => {
     const parlorId = +(params.parlor ?? NaN)
@@ -10,15 +10,15 @@ export const POST = async ({ cookies, params }) => {
         error(400, 'Invalid parlor')
     }
 
-    const sessionId = getSessionId(cookies);
-    const currentUser = await getUser(sessionId);
+    const sessionId = getSessionId(cookies)
+    const currentUser = await getUser(sessionId)
     if (!currentUser) {
-        error(401, 'Unauthorized to perform this action.');
+        error(401, 'Unauthorized to perform this action.')
     }
 
     const isAdmin = await isParlorAdmin(currentUser.id, parlorId)
     if (!isAdmin) {
-        error(401, 'Unauthorized to perform this action.');
+        error(401, 'Unauthorized to perform this action.')
     }
-    return new Response();
+    return new Response()
 }
